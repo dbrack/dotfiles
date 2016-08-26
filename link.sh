59ad3e6e5009b89b5867fbcf36500789498450fa
Dominik Brack <dominik.brack@gmail.com>
2016-08-26 13:32:08 +0200

    Changed links to encrypted files

--- link.sh
+++ link.sh
@@ -17,9 +17,9 @@ ln -s $PWD/ZSH\ Themes/manu.zsh-theme ~/.oh-my-zsh/themes/manu.zsh-theme
 # link google drive folders
 ln -s ~/Google\ Drive/Documents/Education ~/Documents
 ln -s ~/Google\ Drive/Documents/Miscellaneous ~/Documents
-ln -s /Volumes/Boxcryptor/Google\ Drive/Documents/Private ~/Documents
+ln -s /Volumes/Secomba/Dominik/Boxcryptor/Google\ Drive/Documents/Private ~/Documents
 ln -s ~/Google\ Drive/Pictures/Wallpaper ~/Pictures/Wallpaper
-ln -s /Volumes/Boxcryptor/Google\ Drive/Pictures/Celebrities/ ~/Pictures/Celebrities
+ln -s /Volumes/Secomba/Dominik/Boxcryptor/Google\ Drive/Pictures/Celebrities ~/Pictures
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
