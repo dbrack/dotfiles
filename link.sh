4bd7084864e2f4129dc1d2deaca71f7e2eadc798
Dominik Brack <dominik.brack@gmail.com>
2018-12-31 20:45:47 +0100

    Update user name

--- link.sh
+++ link.sh
@@ -21,8 +21,8 @@ ln -s $PWD/hyper.js ~/.hyper.js
 ln -s ~/Google\ Drive/Documents/Education ~/Documents
 ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/Miscellaneous/ ~/Documents
 ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Pictures/Wallpaper ~/Pictures
-ln -s /Volumes/Secomba/Dominik/Boxcryptor/iCloud/Private/ ~/Documents
-ln -s /Volumes/Secomba/Dominik/Boxcryptor/iCloud/Celebrities/ ~/Pictures
+ln -s /Volumes/Secomba/dominik/Boxcryptor/iCloud/Private/ ~/Documents
+ln -s /Volumes/Secomba/dominik/Boxcryptor/iCloud/Celebrities/ ~/Pictures
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
