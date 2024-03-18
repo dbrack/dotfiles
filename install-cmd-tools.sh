7072ca63361a2407a09692e566f38d0f58791b2b
Dominik Brack <dominik.brack@gmail.com>
2024-03-18 18:54:39 +0100

    Add new script to install command line utils

---
+++ install-cmd-tools.sh
@@ -0,0 +1,8 @@
+# # install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# # install volta
+curl https://get.volta.sh | sh
+
+# # install rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
