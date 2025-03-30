2dc6e31776ee223e790130e7a03aad0e9b38356b
Dominik Brack <315219+dbrack@users.noreply.github.com>
2025-03-30 15:32:39 +0200

    Add bun installation script

--- install-cmd-tools.sh
+++ install-cmd-tools.sh
@@ -1,8 +1,11 @@
-# # install oh-my-zsh
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# # install volta
+# install volta
 curl https://get.volta.sh | sh
 
-# # install rustup
+# install bun
+curl -fsSL https://bun.sh/install | bash
+
+# install rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
